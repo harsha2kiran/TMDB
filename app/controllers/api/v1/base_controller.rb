7f23f86f545e4684f5da71e452612b1025b2c88a
Vlatko Trajkov <vt@novp.com>
2013-08-08 20:01:13 +0200

    auto add approve=false if user is not admin

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -5,11 +5,13 @@ class Api::V1::BaseController < ApplicationController
   before_filter :set_controller_name
   # before_filter :check_if_authenticated
   before_filter :set_params_user_id
+  before_filter :set_approved_false
 
   private
 
   def set_controller_name
     @controller = params[:controller].gsub("api/v1/", "")
+    @attribute_names = @controller.classify.constantize.attribute_names
   end
 
   def check_if_authenticated
@@ -24,19 +26,30 @@ class Api::V1::BaseController < ApplicationController
   end
 
   def set_params_user_id
-    attribute_names = @controller.classify.constantize.attribute_names
     if params[:action] == "create"
-      if current_user && attribute_names.include?("user_id")
+      # add user_id value from current_user or redirect to login.
+      # check if current model has attribute user_id, if not, skip it
+      if current_user && @attribute_names.include?("user_id")
         params["#{@controller.singularize.to_sym}"][:user_id] = current_user.id
       else
         # comment this
-        if attribute_names.include?("user_id")
+        if @attribute_names.include?("user_id")
           params["#{@controller.singularize.to_sym}"][:user_id] = User.first.id
         end
         # comment end
         #uncomment authenticate_user!
         # authenticate_user!
       end
+    end
+  end
+
+  def set_approved_false
+    #TODO remove next line
+    current_user = User.first
+    # add approved = false value to record on create if column excist
+    #TODO uncomment last part of next row
+    if params[:action] == "create" && @attribute_names.include?("approved") # && current_user.user_type != "admin"
+      params["#{@controller.singularize.to_sym}"][:approved] = false
     end
   end
 
--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,8 +2,4 @@ class Api::V1::MoviesController < Api::V1::BaseController
 
   inherit_resources
 
-  def create
-    logger.info params.to_yaml
-    render nothing: true
-  end
 end
