9e31b263580abe6e33960ebf6413e7817ca9c119
Janmejay Rai <janmejay.rai@rsystems.com>
2014-03-12 13:26:33 +0530

    added secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Movies::Application.config.secret_token = '0d10870462ee2b801fca7fc18d41e8c3423b9163419d671b60282b4494b170dc0a82a6be3d934580d2a7404f6278a436c67297751f36093440c7c5fa80458909'
+Movies::Application.config.secret_key_base = 'f73ac60af60ae6b84e1925dc697da17f65c6a302b6a5eb59d54b7d9919a8d531a25ad10528d94702833a435508c95b14a3e51d2f938118deb651cf61f7a548df'
