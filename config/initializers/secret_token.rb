fea37f77b12bdd0950d05474086be35b5bd68d28
Gabriel <gabriel85327@gmail.com>
2014-03-27 03:13:01 +0200

    new update rails4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,8 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Movies::Application.config.secret_token = '0d10870462ee2b801fca7fc18d41e8c3423b9163419d671b60282b4494b170dc0a82a6be3d934580d2a7404f6278a436c67297751f36093440c7c5fa80458909'
-Movies::Application.config.secret_key_base = '3bedaac8aad73297cf01efacb45d2ba98899f87fe6a36ca82b4b2e908a87f720e5177cb29fd4cb595a64710345ab02e3c283332dec01aec757a26b053c7fb7c5'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Movies::Application.config.secret_key_base = '3c52f355c5e5f5c7df8c86017933af88857dd02198e1a0a63548ebc88c0bcd9fde9103c27d8657f7d58b6163ef9135088555912814d6ee9240f5f2a5f4d831e7'
